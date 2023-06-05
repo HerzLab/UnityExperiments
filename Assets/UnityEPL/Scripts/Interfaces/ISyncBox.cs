b2da3fd4b9a0444debcf776c16376a2de43edf62
James Bruska <jamesbthecoder@gmail.com>
2023-06-05 16:11:26 -0400

    Moved UnityEPL into a package

--- Assets/UnityEPL/Scripts/Interfaces/ISyncBox.cs
+++
@@ -1,6 +0,0 @@
-public interface ISyncBox {
-    void Init();
-    bool IsRunning();
-    void StartPulse();
-    void StopPulse();
-}
--- Assets/UnityEPL/Scripts/Utilities/UnityUtilities.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public static class UnityUtilities {
-    public static bool IsMacOS() {
-        return Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer;
-    }
-}
