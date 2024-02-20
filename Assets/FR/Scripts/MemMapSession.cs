aced46f53834f161e78255ff9238c62d00aec632
James Bruska <jamesbthecoder@gmail.com>
2024-02-20 12:58:36 -0500

    Forgot to include MemMapSession for quite a few commits now

---
+++ Assets/FR/Scripts/MemMapSession.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using UnityEPL;
+
+namespace UnityEPL {
+
+    public class MemMapTrial<T> : FRRun<T> 
+        where T : PairedWord
+    {
+        public StimWordList<T> recog;
+        public bool recogStim;
+
+        public MemMapTrial(StimWordList<T> encodingList, StimWordList<T> recallList, StimWordList<T> recogList,
+            bool setEncodingStim = false, bool setRecallStim = false, bool setRecogStim = false) :
+            base(encodingList, recallList, setEncodingStim, setRecallStim)
+        {
+            recog = recogList;
+            recogStim = setRecogStim;
+        }
+    }
+
+    [Serializable]
+    public class MemMapSession<T> : FRSessionBase<T, MemMapTrial<T>> 
+        where T : PairedWord 
+    {
+        public MemMapTrial<T> GetTrial() {
+            return GetState();
+        }
+
+        private static string StimWordToWord(T sw) {
+            return sw.word;
+        }
+
+        public void DebugPrintAll() {
+            string output = "";
+            foreach (var trial in this) {
+                // Print the stim type
+                if (trial.encodingStim && trial.recallStim) { output += "B"; }
+                else if (trial.encodingStim) { output += "E"; }
+                else if (trial.recallStim) { output += "R"; }
+                else { output += "N"; }
+                output += " - ";
+
+                // Print the paired word order
+                List<string> recallWords = new();
+                foreach (var stimWord in trial.recall) {
+                    recallWords.Add(stimWord.word.word);
+                }
+                for (int i = 0; i < trial.encoding.Count; ++i) {
+                    output += recallWords.Contains(trial.encoding[i].word.word) ? "T" : "F";
+                }
+                output += " - ";
+
+                // Print the recog order
+                for (int i = 0; i < trial.recog.Count; ++i) {
+                    var recogWord = trial.recog[i].word;
+                    for (int j = 0; j < trial.encoding.Count; ++j) {
+                        if (recogWord.word == trial.encoding[j].word.word || recogWord.word == trial.encoding[j].word.pairedWord) {
+                            output += i + ",";
+                        }
+                    }
+                }
+                output = output.Remove(output.Length-1);
+                output += " - ";
+
+                // Print all the words for encoding, recall, and recognition
+                output += "[";
+                foreach (var stimWord in trial.encoding) {
+                    output += $"({stimWord.word.word}, {stimWord.word.pairedWord}) ";
+                }
+                output = output.Remove(output.Length-1);
+                output += "] - [";
+                foreach (var stimWord in trial.recall) {
+                    output += $"{stimWord.word.word} ";
+                }
+                output = output.Remove(output.Length-1);
+                output += "] - [";
+                foreach (var stimWord in trial.recog) {
+                    output += $"{stimWord.word.word} ";
+                }
+                output = output.Remove(output.Length-1);
+                output += "]";
+
+                output += "\n";
+            }
+            UnityEngine.Debug.Log(output);
+        }
+    }
+
+}
